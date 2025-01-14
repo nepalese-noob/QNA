6c8ce1cf13356986adb32d860fafc8ee0794ecb6
nepalese-noob <85428811+nepalese-noob@users.noreply.github.com>
2025-01-14 16:47:47 +0545

    Update qna.py

--- qna.py
+++ qna.py
@@ -146,5 +146,5 @@ def telegram_webhook():
 
 # Main App Entry Point
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
     
